c277d910e9cb707104cd564990523e898d798ff6
Wesley Hall <wesley.hall@gmail.com>
2022-12-06 20:04:38 +0000

    Fixed deprecated function call

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-(set-default-font "Monospace-18")
+(set-frame-font "Monospace-18")
 
 (eval-after-load 'clojure-mode
   '(define-clojure-indent
@@ -72,8 +72,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (clojure-mode terraform-mode solarized-theme rainbow-delimiters projectile paredit neotree magit exec-path-from-shell company cider better-defaults all-the-icons))))
+   '(clojure-mode terraform-mode solarized-theme rainbow-delimiters projectile paredit neotree magit exec-path-from-shell company cider better-defaults all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
